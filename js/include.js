14289aef4ee550810e3fa70cb8901fadad2e6e54
khushiy05 <khushiyewale150@gmail.com>
2026-08-13 01:44:09 +0530

    edit

--- js/include.js
+++ js/include.js
@@ -1,7 +1,26 @@
 function includeHTML(id, file) {
-  fetch(file)
-    .then(res => res.text())
-    .then(html => document.getElementById(id).innerHTML = html);
+    fetch(file)
+        .then(res => res.text())
+        .then(html => {
+            document.getElementById(id).innerHTML = html;
+
+            // Highlight current page
+            if (id === "header") {
+                let currentPage = window.location.pathname.split("/").pop();
+
+                if (currentPage === "") {
+                    currentPage = "index.html";
+                }
+
+                document.querySelectorAll("#header .nav-link").forEach(link => {
+                    let linkPage = link.getAttribute("href");
+
+                    if (linkPage === currentPage) {
+                        link.classList.add("active");
+                    }
+                });
+            }
+        });
 }
 
 includeHTML("header", "header.html");
