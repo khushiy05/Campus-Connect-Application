675f894ca8b5beded2321be563f2a03d5bacfa38
khushiy05 <khushiyewale150@gmail.com>
2026-09-02 09:51:00 +0530

    SSMS

--- admin-source/src/views/Others/RojgarSetu.vue
+++ admin-source/src/views/Others/RojgarSetu.vue
@@ -987,7 +987,7 @@ async function submitForm() {
   statusMessage.value = "";
 
   try {
-    const res = await fetch("/api/rojgarsetu", {
+    const res = await fetch("http://127.0.0.1:5000/api/rojgarsetu", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(form),
