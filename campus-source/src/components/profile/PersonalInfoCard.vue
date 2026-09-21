49efeb48ff3fc7656f2c9c76781b4242d7a6a7c1
khushiy05 <khushiyewale150@gmail.com>
2026-09-21 15:21:02 +0530

    campus panel

--- campus-source/src/components/profile/PersonalInfoCard.vue
+++ campus-source/src/components/profile/PersonalInfoCard.vue
@@ -9,13 +9,8 @@
 
           <div class="grid grid-cols-1 gap-4 lg:grid-cols-2 lg:gap-7 2xl:gap-x-32">
             <div>
-              <p class="mb-2 text-xs leading-normal text-gray-500 dark:text-gray-400">First Name</p>
-              <p class="text-sm font-medium text-gray-800 dark:text-white/90">Musharof</p>
-            </div>
-
-            <div>
-              <p class="mb-2 text-xs leading-normal text-gray-500 dark:text-gray-400">Last Name</p>
-              <p class="text-sm font-medium text-gray-800 dark:text-white/90">Chowdhury</p>
+              <p class="mb-2 text-xs leading-normal text-gray-500 dark:text-gray-400">Name</p>
+              <p class="text-sm font-medium text-gray-800 dark:text-white/90">{{ profile?.name }}</p>
             </div>
 
             <div>
@@ -23,18 +18,28 @@
                 Email address
               </p>
               <p class="text-sm font-medium text-gray-800 dark:text-white/90">
-                randomuser@pimjo.com
+                {{ profile?.email }}
               </p>
             </div>
 
             <div>
-              <p class="mb-2 text-xs leading-normal text-gray-500 dark:text-gray-400">Phone</p>
-              <p class="text-sm font-medium text-gray-800 dark:text-white/90">+09 363 398 46</p>
+              <p class="mb-2 text-xs leading-normal text-gray-500 dark:text-gray-400">Mobile</p>
+              <p class="text-sm font-medium text-gray-800 dark:text-white/90">{{ profile?.mobile }}</p>
             </div>
 
             <div>
-              <p class="mb-2 text-xs leading-normal text-gray-500 dark:text-gray-400">Bio</p>
-              <p class="text-sm font-medium text-gray-800 dark:text-white/90">Team Manager</p>
+              <p class="mb-2 text-xs leading-normal text-gray-500 dark:text-gray-400">City</p>
+              <p class="text-sm font-medium text-gray-800 dark:text-white/90">{{ profile?.city }}</p>
+            </div>
+
+            <div>
+              <p class="mb-2 text-xs leading-normal text-gray-500 dark:text-gray-400">College Name</p>
+              <p class="text-sm font-medium text-gray-800 dark:text-white/90">{{ profile?.college_name }}</p>
+            </div>
+
+            <div>
+              <p class="mb-2 text-xs leading-normal text-gray-500 dark:text-gray-400">College Code</p>
+              <p class="text-sm font-medium text-gray-800 dark:text-white/90">{{ profile?.college_code }}</p>
             </div>
           </div>
         </div>
@@ -253,6 +258,13 @@
 <script setup>
 import { ref } from 'vue'
 import Modal from './Modal.vue'
+
+const props = defineProps({
+  profile: {
+    type: Object,
+    default: () => ({}),
+  },
+})
 
 const isProfileInfoModal = ref(false)
 
